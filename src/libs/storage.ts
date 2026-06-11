be07915700017e85b3e47ea358f8778c38ceedeb
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-11 07:54:14 -0300

    feat: implement mmkvZustandStorage for state management

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -1,5 +1,12 @@
 import { createMMKV } from "react-native-mmkv";
+import { StateStorage } from "zustand/middleware";
 
 export const storage = createMMKV({
   id: "voice-vault",
 });
+
+export const mmkvZustandStorage: StateStorage = {
+  setItem: (key, value) => storage.set(key, value),
+  getItem: (key) => storage.getString(key) ?? null,
+  removeItem: (key) => storage.remove(key),
+};
