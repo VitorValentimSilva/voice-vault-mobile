6f05078b48cba0929ef99bd80c783f74ca4495e9
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-12 19:15:07 -0300

    feat: add notification handler with configuration options

---
+++ src/shared/constants/notification.ts
@@ -0,0 +1,8 @@
+export const NOTIFICATION_HANDLER = {
+  handleNotification: async () => ({
+    shouldShowBanner: true,
+    shouldShowList: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+};
