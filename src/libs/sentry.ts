1ae16c781d39abf236a9b3d7c5a6997a9d4e130c
Vitor Valentim da Silva Ferreira <vitorvalentin840@gmail.com>
2026-06-11 23:29:29 -0300

    fix: adjust Sentry configuration for traces and profiles sampling rates

--- src/libs/sentry.ts
+++ src/libs/sentry.ts
@@ -2,6 +2,8 @@ import * as Sentry from '@sentry/react-native';
 
 Sentry.init({
   dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
-  tracesSampleRate: 1.0,
   enabled: !__DEV__,
+  tracesSampleRate: 0.2,
+  profilesSampleRate: 0.1,
+  sendDefaultPii: false,
 });
